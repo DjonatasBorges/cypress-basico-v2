708a758fb4697e058107227bf95e5c376b25b3cc
DjonatasBorges <interdjonatas@gmail.com>
2022-09-08 07:32:40 -0300

    Aula2. Exercício

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -7,9 +7,24 @@
 
 /// <reference types="Cypress" />
 
-describe('Central de Atendimento ao Cliente TAT', function() {
+
+
+describe('Hooks', () => { describe('Central de Atendimento ao Cliente TAT', function() {
     it('verifica o título da aplicação', function() {
         cy.visit('./src/index.html')
         cy.title().should('be.equals', 'Central de Atendimento ao Cliente TAT')
+    })
+  })
+})
+
+
+describe('Envio de Formulário', function() {
+    it('envia um formulário', function() {
+        cy.get('#firstName').should('be.visible').type('Djonatas')
+        cy.get('#lastName').should('be.visible').type('Borges')
+        cy.get('#email').should('be.visible').type('interdjonatas@gmail.com')
+        cy.get('#open-text-area').should('be.visible').type('VERIFICANDO SE O TESTE ESTA DIGITANDO TUDO.')
+        cy.get('.button').click()
+        cy.get('.success').should('be.visible')
     })
   })
